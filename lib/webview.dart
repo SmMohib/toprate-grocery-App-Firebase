e352a7460ec037eaebf4914b8b83167f62739268
MOHIB <mohib9419@gmail.com>
2023-02-23 23:41:06 +0600

    webview

--- lib/webview.dart
+++ lib/webview.dart
@@ -2,11 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class WebViewsScreen extends StatelessWidget {
-  const WebViewsScreen({super.key});
+// class NavigationControls extends StatelessWidget {
+//   const NavigationControls({super.key, required this.webViewController});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
-  }
-}
+//   final WebViewController webViewController;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: <Widget>[
+//         IconButton(
+//           icon: const Icon(Icons.arrow_back_ios),
+//           onPressed: () async {
+//             if (await webViewController.canGoBack()) {
+//               await webViewController.goBack();
+//             } else {
+//               if (context.mounted) {
+//                 ScaffoldMessenger.of(context).showSnackBar(
+//                   const SnackBar(content: Text('No back history item')),
+//                 );
+//               }
+//             }
+//           },
+//         ),
+//         IconButton(
+//           icon: const Icon(Icons.arrow_forward_ios),
+//           onPressed: () async {
+//             if (await webViewController.canGoForward()) {
+//               await webViewController.goForward();
+//             } else {
+//               if (context.mounted) {
+//                 ScaffoldMessenger.of(context).showSnackBar(
+//                   const SnackBar(content: Text('No forward history item')),
+//                 );
+//               }
+//             }
+//           },
+//         ),
+//         IconButton(
+//           icon: const Icon(Icons.replay),
+//           onPressed: () => webViewController.reload(),
+//         ),
+//       ],
+//     );
+//   }
+// }
