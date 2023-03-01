4232b0bd127d627ca3df20533102e61e23acf07c
MOHIB <mohib9419@gmail.com>
2023-03-02 01:06:50 +0600

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:iconly/iconly.dart';
 import 'package:provider/provider.dart';
 import 'package:toprate/consts/colors.dart';
@@ -32,74 +33,88 @@ class _HomeScreenState extends State<HomeScreen> {
     final themeState = Provider.of<DarkThemeProvider>(context);
     Size size = Utils(context).sizeScreen;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
+        body: SingleChildScrollView(
+            child: Column(children: [
+      SizedBox(
+        height: size.height * 0.33,
+        child: Swiper(
+          itemCount: imagelist.length,
+          itemBuilder: (context, index) {
+            return Image.asset(
+              imagelist[index],
+              fit: BoxFit.cover,
+            );
+          },
+          autoplay: true,
+          pagination: SwiperPagination(
+              alignment: Alignment.bottomCenter,
+              builder: DotSwiperPaginationBuilder(
+                color: Colors.white,
+                activeColor: Colors.red,
+              )),
+          control: SwiperControl(color: Colors.black),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            SizedBox(
-              height: size.height * 0.33,
-              child: Swiper(
-                itemCount: imagelist.length,
-                itemBuilder: (context, index) {
-                  return Image.asset(
-                    imagelist[index],
-                    fit: BoxFit.cover,
-                  );
-                },
-                autoplay: true,
-                pagination: SwiperPagination(
-                    alignment: Alignment.bottomCenter,
-                    builder: DotSwiperPaginationBuilder(
-                      color: Colors.white,
-                      activeColor: Colors.red,
-                    )),
-                control: SwiperControl(color: Colors.black),
-              ),
+            // ignore: prefer_const_constructors
+            Text(
+              'Resent Added',
+              style: TextStyle(fontSize: 20),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // ignore: prefer_const_constructors
-                  Text(
-                    'Resent Added',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'See All',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: themeState.getDarkTheme
-                                ? Colors.black
-                                : Colors.white),
-                      ))
-                ],
-              ),
-            ),
-
-            //recent add
-            SizedBox(
-                height: size.width * 0.80,
-                child: ListView.builder(
-                    physics: BouncingScrollPhysics(),
-                    itemCount: imagelist.length,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return ItemWidget();
-                    })),
-
-            //   Image.asset(
-            //     'images/warning-sign.png',
-            //     height: 200,
-            //     width: 200,
-            //   ),
-            //  Image.network(
-            //               "https://cdn.britannica.com/39/187439-050-35BA4DCA/Broccoli-florets.jpg"),
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  'See All',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: themeState.getDarkTheme
+                          ? Colors.black
+                          : Colors.white),
+                ))
           ],
         ),
       ),
-    );
+
+      //recent add
+      Row(
+        children: [
+          RotatedBox(
+            quarterTurns: -1,
+            child: Row(
+              children: [
+                TextWidget(
+                  text: 'ON SALE',
+                  textSize: 30,
+                  color: Colors.red,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Icon(
+                  IconlyLight.discount,
+                  color: Colors.red,
+                )
+              ],
+            ),
+          ),
+          Flexible(
+            child: SizedBox(
+              height: size.height * 0.25,
+              child: ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  itemCount: imagelist.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return ItemWidget();
+                  }),
+            ),
+          ),
+        ],
+      ),
+    ])));
   }
 }
